c2e102a093394f2dcf0025b77b9eb1f786c8d25b
Daniel Strielnikow <daniel.strielnikow@outlook.com>
2026-08-27 12:33:56 +0200

    Config and connect MCU to ESP32

--- src/main.c
+++ src/main.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
+#include "driver/gpio.h"
+#include "driver/i2c_master.h"
 
+#define PIN_I2C_SCL_GPIO_NUM_22 GPIO_NUM_22
+#define PIN_I2C_SDA_GPIO_NUM_21 GPIO_NUM_21
+const uint8_t MCU_6050_ADDR = 0x68;
+const uint8_t MCU_6050_WAKEUP = 0x6B;
+const uint8_t MCU_6050_WAKEUP_DATA = 0x00;
 
-#define I2C_MASTER_SCL_IO   22
-#define I2C_MASTER_SDA_IO   21
-#define I2C_MASTER_NUM      I2C_NUM_0
-#define I2C_MASTER_FREQ_HZ  100000
-#define I2C_TIMEOUT_MS      1000
+i2c_master_bus_config_t i2c_mst_config = {
+    .clk_source = I2C_CLK_SRC_DEFAULT,
+    .i2c_port = -1,
+    .scl_io_num = GPIO_NUM_22,
+    .sda_io_num = GPIO_NUM_21,
+    .glitch_ignore_cnt = 7,
+    .flags.enable_internal_pullup = true,
+};
 
+i2c_master_bus_handle_t bus_handle;
+i2c_master_dev_handle_t dev_handle;
 
-#define MCU6050_REGISTRY    0x68
-#define MCU6050_POWER       0x6B
-#define MCU6050_ACCEL       0x3B
+i2c_device_config_t dev_config = {
+    .device_address = MCU_6050_ADDR,
+    .scl_speed_hz = 400000,
+};
 
+void app_main() {
+    i2c_new_master_bus(&i2c_mst_config, &bus_handle);
+    i2c_master_bus_add_device(bus_handle, &dev_config, &dev_handle);
 
+    uint8_t wakeUpMCU[] = {MCU_6050_WAKEUP, MCU_6050_WAKEUP_DATA};
 
-void setup()
-{
+    i2c_master_transmit(dev_handle, wakeUpMCU, sizeof(wakeUpMCU), -1);
 
 
 }
+
+
+
+
+
+
+
